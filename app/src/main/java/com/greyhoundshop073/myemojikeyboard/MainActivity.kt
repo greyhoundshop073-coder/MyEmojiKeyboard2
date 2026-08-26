8afc7266eb5584af72772ec17b88f9a122766bd8
greyhoundshop073-coder <greyhoundshop073@gmail.com>
2026-08-26 10:48:29 +0100

    Add MainActivity for emoji keyboard application

---
+++ app/src/main/java/com/greyhoundshop073/myemojikeyboard/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.greyhoundshop073.myemojikeyboard
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
