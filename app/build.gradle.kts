69f601911cca5158826e14a3d94cb1cd1408cef0
greyhoundshop073-coder <greyhoundshop073@gmail.com>
2026-08-26 10:34:52 +0100

    Add initial build configuration for Android app

---
+++ app/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.greyhoundshop073.myemojikeyboard"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.greyhoundshop073.myemojikeyboard"
+        minSdk = 26
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0.0"
+    }
+
+    buildFeatures {
+        buildConfig = true
+    }
+}
+
+kotlin {
+    jvmToolchain(17)
+}
